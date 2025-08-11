8ee4a5deeef72b77c4bb9bd20b17be0981b176dd
trisdoan <doanminhtri8183@gmail.com>
2025-08-11 17:46:35 +0700

    [IMP] refactor Front-end code
    # changes
    1.

---
+++ hr_governance/static/src/components/splitter.esm.js
@@ -0,0 +1,31 @@
+import {Component, onWillUnmount, useExternalListener, xml} from "@odoo/owl";
+
+export class Splitter extends Component {
+    static template = xml`<div class="o_governance_splitter" t-on-mousedown="startResizing"/>`;
+
+    static props = {
+        onResize: Function,
+    };
+
+    setup() {
+        this.isResizing = false;
+        useExternalListener(window, "mousemove", this.onMouseMove.bind(this));
+        useExternalListener(window, "mouseup", this.stopResizing.bind(this));
+    }
+
+    startResizing(ev) {
+        ev.preventDefault();
+        this.isResizing = true;
+    }
+
+    onMouseMove = (ev) => {
+        if (!this.isResizing) return;
+        if (this.props.onResize) {
+            this.props.onResize(ev);
+        }
+    };
+
+    stopResizing = () => {
+        this.isResizing = false;
+    };
+}
